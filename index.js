2223683e1038dc5925020d11d9b6727b7ea5fadf
Steve Molele <stevemolele@yahoo.com>
2022-04-01 03:54:06 +0200

    finished crew page

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelectorAll('[role="tab"]');
-
-let tabFocus = 0;
-
 navToggle.addEventListener("click", () => {
   const visibility = nav.getAttribute("data-visible");
   if (visibility === "false") {
@@ -16,60 +11,3 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
-
-tabList.addEventListener("keydown", (e) => {
-  const keydownLeft = 37;
-  const keydownRight = 39;
-
-  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-    tabs[tabFocus].setAttribute("tabindex", -1);
-
-    if (e.keyCode === keydownRight) {
-      tabFocus++;
-      if (tabFocus >= tabs.length) {
-        tabFocus = 0;
-      }
-    } else if (e.keyCode === keydownLeft) {
-      tabFocus--;
-      if (tabFocus < 0) {
-        tabFocus = tabs.length - 1;
-      }
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
-  }
-});
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    const targetTab = e.target;
-    const targetPanel = targetTab.getAttribute("aria-controls");
-    const targetImage = targetTab.getAttribute("data-image");
-
-    const tabContainer = targetTab.parentNode;
-    const mainContainer = tabContainer.parentNode;
-
-    tabContainer
-      .querySelector('[aria-selected="true"]')
-      .setAttribute("aria-selected", false);
-
-    targetTab.setAttribute("aria-selected", true);
-
-    hideContent(mainContainer, '[role="tabpanel"]');
-    showContent(mainContainer, [`#${targetPanel}`]);
-
-    hideContent(mainContainer, "picture");
-    showContent(mainContainer, [`#${targetImage}`]);
-  });
-});
-
-function hideContent(parent, content) {
-  parent
-    .querySelectorAll(content)
-    .forEach((item) => item.setAttribute("hidden", true));
-}
-
-function showContent(parent, content) {
-  parent.querySelector(content).removeAttribute("hidden");
-}
